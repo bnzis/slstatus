e926a1ebd398d2e6141747a6877f10a759eeaa15
Matthew Parnell <matt@parnmatt.co.uk>
2018-03-05 17:12:20 +0000

    Add some padding

--- config.h
+++ config.h
@@ -13,12 +13,12 @@ static const char unknown_str[] = "n/a";
 /* output formats */
 static const struct arg args[] = {
 	/* function, format, argument */
-	{ disk_free, "%.5s GiB | ", "/" },
+	{ disk_free, " %.5s GiB | ", "/" },
 	{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
 	{ wifi_essid, "%s) ", "wlp8s0" },
 	{ ipv4, "%s | ", "wlp8s0" },
 	{ run_command, "V: %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ battery_state, "%s ", "BAT1" },
 	{ battery_perc, "%3s%% | ", "BAT1" },
-	{ datetime, "%s", "[%a] %F %T %Z" },
+	{ datetime, "%s", "[%a] %F %T %Z " },
 };
